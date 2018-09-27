9bf1fb03930becdcf8abade9db3b1104e0d3c886
Suspiciousfish <Dtgi331@gmail.com>
2018-09-27 09:14:55 -0400

    finished john quincy adding machine , starting on basic calculator

--- JQA.js
+++ JQA.js
@@ -1,42 +1,34 @@
 /* 
-Logic replicator
+adding machine
 */
-
+const math = require('mathjs')
+const chalk = require('chalk');
+var numberIsNan = require('number-is-nan');
 var prompt = require ('prompt');
 
 prompt.start();
 
-Number(prompt.get(['A', 'B', "operator"], function JQA(err,result){
+Number(prompt.get(['A', 'B', 'OP'], function JQA(err,result){
 	var A;
 	var B;
-	var z;
+	var Q
 		console.log("A is " + result.A);
- 		console.log("B is " + result.B);	
-			var C = result.A;
-			var D = result.B;
+ 		console.log("B is " + result.B);
+ 		console.log("Operation is " + result.OP)	
+			var C = parseFloat(result.A);
+			var D = parseFloat(result.B);
+				console.log(C+D);
+				//console.log(result)
+var Z = (C+D)
 
+//===========================================================================================================//
 
+if (numberIsNan(Z)){
+		console.log(chalk.red('error! INPUT NUMBERS'))
 
+	  } else {
+	  		console.log(chalk.green('thank you for not putting in letters or special characters =^.^= '))
+	  }
 
-		console.log("your operation is " + result.operator);
-
-	// function OP(C,D){
-	// 		if (result.operator = "+"){
-	// 			return (C + D);
-	// 			}
-			// else(result.operator = "-"){
-			// 	return (C - D);
-			// 	}
-			// else(result.operator = "*"){
-			// 	return (C * D);
-			// 	}
-			// else(result.operator = "/"){
-			// 	return (C / D);
-			// 	}
-	// 		console.log(" your Result is " , Op() );
-	// 		}
-
-}))
-
-// need }))
+}));
 
